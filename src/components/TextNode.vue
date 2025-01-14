effece1e5b87beeca436bf3e6f70b7b1c4b71972
Pedro Luiz <p3dr0c4@live.com>
2025-01-14 13:36:53 -0300

    wip: create TextNode

---
+++ src/components/TextNode.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="text_node">
+        <textarea
+            :value="content"
+            @input="onInput"
+        ></textarea>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, ref } from 'vue';
+import type { TextNode } from '../core/HtmlNode';
+import type { HtmlNodeProps } from '../core/BlockNode';
+type TextNodeProps = HtmlNodeProps<TextNode>
+
+
+const props = defineProps<TextNodeProps & {content: string}>();
+const content = ref(props.content);
+const emit = defineEmits(['onContentChange']);
+
+const onInput = (event: Event) => {
+    const target = event.target as HTMLTextAreaElement
+    content.value = target.value;
+    emit('onContentChange', target.value);
+};
+</script>
+
+<style scoped>
+.text_node textarea {
+    width: 100%;
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+</style>
--- src/core/BlockNode.ts
+++ src/core/BlockNode.ts
@@ -1,7 +1,7 @@
 import type { HtmlNode, ImageNode, TextNode } from "./HtmlNode";
 import { generateRandomStringKey } from "./StringUtils";
 export type BlockNode = ImageNode | TextNode;
-export type HtmlNodeProps<Node extends HtmlNode> = Pick<Node, "htmlTag" | "attributes">
+export type HtmlNodeProps<Node extends HtmlNode> = Pick<Node, "htmlTag" | "attributes" | "content">
 export type ComponentNodeProps<T extends HtmlNode> = HtmlNodeProps<T> & {key: string}
 export type BlockNodeProps = ComponentNodeProps<BlockNode>
 export const createNodeBlock = <T extends HtmlNode>(nodeProps: HtmlNodeProps<T>): ComponentNodeProps<T> => {
@@ -13,6 +13,7 @@ export const createNodeBlock = <T extends HtmlNode>(nodeProps: HtmlNodeProps<T>)
 
 export const createImageNodeBlock = (): ComponentNodeProps<ImageNode> => createNodeBlock({
     htmlTag: "img",
+    content: undefined,
     attributes: {
       src: "",
       alt: "",
@@ -22,5 +23,6 @@ export const createImageNodeBlock = (): ComponentNodeProps<ImageNode> => createN
   });
 export const createTextNodeBlock = (): ComponentNodeProps<TextNode> => createNodeBlock({
     htmlTag: "p",
-    attributes: {}
+    attributes: {},
+    content: ""
 })
